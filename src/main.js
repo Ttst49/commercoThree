4804af3dbc0b563c2fafbda8c3af177a99ba283c
Ttst <thibautstachnick@gmail.com>
2024-03-21 16:57:38 +0100

    update index and main

--- src/main.js
+++ src/main.js
@@ -1,27 +1,31 @@
 import * as THREE from "three";
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 import {OrbitControls} from "three/addons";
 
 const container = document.getElementById('hero');
-
-
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-
 const renderer = new THREE.WebGLRenderer();
+const ambientLight = new THREE.AmbientLight(0xFFFFFF,1)
+scene.add(ambientLight);
+let actualModel
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight/2 );
+const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight/0.5, 0.1, 1000 );
+camera.updateProjectionMatrix();
 container.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const controls = new OrbitControls(camera,renderer.domElement);
+const loader = new GLTFLoader();
 
-camera.position.z = 5;
+loader.load("../assets/tShirt/scene.gltf", function( gltf ) {
+
+    actualModel = gltf.scene
+    scene.add(gltf.scene)
+})
 
 
 function animate() {
+
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 }
